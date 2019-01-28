ba1469b16637f7e3f3332201c058a7f7f172780e
k03mad <k03.mad@gmail.com>
2019-01-29 02:07:47 +0300

    fix path

--- app/cron.js
+++ app/cron.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const cron = require('node-cron');
-const t = require('require-all')(`${__dirname}/tasks`);
+const t = require('require-all')(`${__dirname}/lib/tasks`);
 
 cron.schedule('0 * * * *', () => t.dns.update());
 cron.schedule('30 5 * * *', () => t.pi.reboot());

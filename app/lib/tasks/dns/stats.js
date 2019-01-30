9f08c82d73d2b252a3e709ef6734a88d3f3c03ba
k03mad <k03.mad@gmail.com>
2019-01-31 00:35:01 +0300

    fix stats

--- app/lib/tasks/dns/stats.js
+++ app/lib/tasks/dns/stats.js
@@ -23,7 +23,7 @@ module.exports = async () => {
         }
 
         if (data.domains_being_blocked !== 0) {
-            await sendToInflux({meas: 'dns', tags: {pihole: 'stats'}, values: body});
+            await sendToInflux({meas: 'dns', tags: {pihole: 'stats'}, values: data});
         }
     } catch (err) {
         log.print(err);

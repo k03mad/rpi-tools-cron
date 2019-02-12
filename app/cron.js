820927b377dbe8628a3a00436111867ec7a03fa8
k03mad <k03.mad@gmail.com>
2019-02-12 19:30:28 +0300

    update cron

--- app/cron.js
+++ app/cron.js
@@ -2,18 +2,29 @@
 
 const cron = require('node-cron');
 const t = require('require-all')(`${__dirname}/lib/tasks`);
+const {array} = require('utils-mad');
 
-cron.schedule('* * * * *', () => Promise.all([
-    t.pi.usage(),
-    t.sensors.weather(),
-]));
+const crons = {
+    // every minute
+    '* * * * *': [
+        t.pi.usage,
+        t.sensors.weather,
+    ],
+    // every N minutes
+    '*/5 * * * *': [
+        t.dns.clients,
+        t.dns.hosts,
+        t.dns.stats,
+        t.dns.time,
+    ],
+    // every N hour, N minute
+    '0 */6 * * *': t.dns.update,
+    // at N hour, N minute
+    '30 5 * * *': t.pi.reboot,
+};
 
-cron.schedule('*/10 * * * *', () => Promise.all([
-    t.dns.clients(),
-    t.dns.hosts(),
-    t.dns.stats(),
-]));
-
-cron.schedule('*/5 * * * *', () => t.dns.time());
-cron.schedule('0 */6 * * *', () => t.dns.update());
-cron.schedule('30 5 * * *', () => t.pi.reboot());
+for (const [key, value] of Object.entries(crons)) {
+    for (const func of array.convert(value)) {
+        cron.schedule(key, () => func());
+    }
+}

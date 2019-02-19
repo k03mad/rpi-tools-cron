a9923b25400fc712896cc377c3f59b8912c55d45
Kirill Molchanov <k03.mad@gmail.com>
2019-02-19 11:25:13 +0300

    cron update

--- app/cron.js
+++ app/cron.js
@@ -5,20 +5,19 @@ const t = require('require-all')(`${__dirname}/lib/tasks`);
 const {array} = require('utils-mad');
 
 const crons = {
-    // every minute
+
     '* * * * *': [
         t.dns.clients,
         t.dns.hosts,
         t.dns.stats,
         t.dns.time,
-        t.pi.ping,
         t.pi.usage,
         t.sensors.weather,
     ],
-    // every N hour, N minute
-    '0 */1 * * *': t.dns.update,
-    // at N hour, N minute
+
+    '0 */3 * * *': t.dns.update,
     '30 5 * * *': t.pi.reboot,
+
 };
 
 for (const [key, value] of Object.entries(crons)) {

97dc4526d74e5f954be12d107df1f3a72c2bcfa0
Kirill Molchanov <k03.mad@gmail.com>
2019-02-19 03:21:51 +0300

    Update usage.js

--- app/lib/tasks/pi/usage.js
+++ app/lib/tasks/pi/usage.js
@@ -4,15 +4,27 @@ const {log, shell} = require('utils-mad');
 const {sendToInflux} = require('../../utils');
 
 /**
- * Get pi cpu usage
+ * Get pi usage
  */
 module.exports = async () => {
     try {
-        const stat = await shell.run('mpstat 1 1');
-        const idle = stat.split('\n')[3].split(' ').pop();
-        const values = {usage: 100 - Number(idle.replace(',', '.'))};
+        const [load, temp, disk, ram] = await Promise.all([
+            shell.run('mpstat 1 1'),
+            shell.run('cat /sys/class/thermal/thermal_zone0/temp'),
+            shell.run('df'),
+            shell.run('free -m'),
+        ]);
 
-        await sendToInflux({meas: 'cpu', values});
+        const cpuIdle = stat.split('\n')[3].split(' ').pop();
+
+        const values = {
+            cpuLoad: 100 - Number(cpuIdle.replace(',', '.')),
+            cpuTemp: Number(temp) / 1000,
+            diskUsage: Number(disk.match(/\/dev\/root +\d+ +(\d+)/)[1]),
+            ramUsage: Number(ram.match(/Mem: +\d+ +(\d+)/)[1]),
+        };
+
+        await sendToInflux({meas: 'pi', values});
     } catch (err) {
         log.print(err);
     }

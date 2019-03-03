d3f0365a1957760c94b894e88ac11c67b5bc579a
k03mad <k03.mad@gmail.com>
2019-03-03 05:27:14 +0300

    sml fix

--- app/tasks/router/usage.js
+++ app/tasks/router/usage.js
@@ -19,9 +19,8 @@ module.exports = async () => {
             '/system/resource/print',
             '/interface/print',
             '/interface/wireless/registration-table/print',
-            ...['wan1', 'ether1', 'ether2', 'ether3', 'ether4', 'wlan1', 'wlan2'].map(
-                elem => ['/interface/monitor-traffic', `=interface=${elem}`, '=once']
-            ),
+            ...['wan1', 'ether1', 'ether2', 'ether3', 'ether4', 'wlan1', 'wlan2']
+                .map(elem => ['/interface/monitor-traffic', `=interface=${elem}`, '=once']),
         ]);
 
         const health = {

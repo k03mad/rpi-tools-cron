f8e7504234216f1b2604df7d1e78fb455319158e
k03mad <k03.mad@gmail.com>
2019-03-04 21:49:15 +0300

    add uptime

--- app/tasks/pi/usage.js
+++ app/tasks/pi/usage.js
@@ -8,18 +8,19 @@ const {sendToInflux} = require('../../lib/utils');
  */
 module.exports = async () => {
     try {
-        const [load, temp, disk, ram] = await Promise.all([
-            shell.run('cat /proc/loadavg'),
+        const [uptime, temp, disk, ram] = await Promise.all([
+            shell.run('uptime'),
             shell.run('cat /sys/class/thermal/thermal_zone0/temp'),
             shell.run('df'),
             shell.run('free -m'),
         ]);
 
         const values = {
-            cpuLoad: Number(load.split(' ')[0]),
+            cpuLoad: Number(uptime.match(/load average: ([\d,]+),/)[1].replace(',', '.')),
             cpuTemp: Number(temp) / 1000,
             diskUsage: Number(disk.match(/\/dev\/root +\d+ +(\d+)/)[1]),
             ramUsage: Number(ram.match(/Mem: +\d+ +(\d+)/)[1]),
+            uptime: uptime.match(/up(.+?),/)[1],
         };
 
         await sendToInflux({meas: 'pi-usage', values});

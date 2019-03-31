4e9b958b94cce12fdd8a3ab9f3d6d1f59db492d1
k03mad <k03.mad@gmail.com>
2019-04-01 00:09:51 +0300

    sml fix

--- app/tasks/router/updates.js
+++ app/tasks/router/updates.js
@@ -5,9 +5,6 @@ const {sendToInflux} = require('../../lib/utils');
 
 module.exports = async () => {
     const [[, updates]] = await getMikrotik('/system/package/update/check-for-updates');
-    console.log('::: --------------------------');
-    console.log('::: my -> updates', updates);
-    console.log('::: --------------------------');
     const values = {
         updates: [
             updates['installed-version'],

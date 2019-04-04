2831ca01faada44fdc9c55a33ffc37ce8f8e58a4
k03mad <k03mad@gmail.com>
2019-04-04 14:50:17 +0300

    parse updates

--- app/tasks/pi/updates.js
+++ app/tasks/pi/updates.js
@@ -3,12 +3,19 @@
 const {shell, influx} = require('utils-mad');
 
 module.exports = async () => {
-    const values = {
-        updates: await shell.run([
-            'sudo apt-get update > /dev/null',
-            'sudo apt-get -u -V upgrade',
-        ]),
-    };
+    const apt = await shell.run([
+        'sudo apt-get update',
+        'sudo apt-get upgrade -u -s',
+    ]);
 
-    await influx.write({meas: 'pi-update', values});
+    const updates = apt
+        .split('\n')
+        .filter(elem => elem.includes('Inst'))
+        .map(elem => {
+            const [, name, current, updated] = elem.match(/Inst (.+) \[(.+)] \((.+?) /);
+            return `${name} ${current} (${updated})`;
+        })
+        .join(' | ');
+
+    await influx.write({meas: 'pi-update', values: {updates}});
 };

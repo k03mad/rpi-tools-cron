5c638c36fc46ffdc1aef49212aa563360731a1ee
k03mad <k03.mad@gmail.com>
2019-05-09 00:56:09 +0300

    add lists count

--- app/tasks/dns/update.js
+++ app/tasks/dns/update.js
@@ -1,8 +1,42 @@
 'use strict';
 
-const {shell} = require('utils-mad');
+const appRoot = require('app-root-path');
+const {influx, shell} = require('utils-mad');
+const {promises: fs} = require('fs');
 
-module.exports = () => shell.script(
-    'adblock-hosts-list',
-    'deploy && pihole -g',
-);
+module.exports = async () => {
+    const REPO = 'adblock-hosts-list';
+    const file = `${REPO}/output/stats.json`;
+
+    await shell.script(REPO, 'deploy && pihole -g');
+    const data = await fs.readFile(`${appRoot}/../${file}`, 'utf8');
+
+    const values = {};
+
+    const clear = [
+        'data|',
+        'hosts|',
+        'master|',
+        'raw.githubusercontent.com|',
+        's3.amazonaws.com|',
+        'bitbucket.org|',
+        'gitlab.com|',
+        /raw\|[\da-f]+\|/,
+        /\.php\|.+/,
+        /\|easylist/g,
+    ];
+
+    Object.entries(JSON.parse(data)).forEach(([key, value]) => {
+        clear.forEach(elem => {
+            key = key.replace(elem, '');
+        });
+
+        if (values[key]) {
+            key = `dup!! ${key}`;
+        }
+
+        values[key.replace(/\|/g, '/')] = value;
+    });
+
+    await influx.write({meas: 'dns-lists', values});
+};

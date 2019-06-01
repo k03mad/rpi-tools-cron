88bb18d748ccdc87cdcf437be9c837c802d473c7
k03mad <k03.mad@gmail.com>
2019-06-01 12:58:58 +0300

    empty network

--- app/tasks/dns/network.js
+++ app/tasks/dns/network.js
@@ -12,10 +12,15 @@ module.exports = async () => {
     for (const elem of network) {
         const {numQueries, ip, name} = elem;
 
-        if (ip.split('.').pop() < DHCP_STATIC_IP && numQueries > 0) {
+        if (
+            ip.split('.').pop() < DHCP_STATIC_IP
+            && numQueries > 0
+        ) {
             values[`${name}|${ip}`] = numQueries;
         }
     }
 
-    await influx.write({meas: 'dns-network', values});
+    if (Object.keys(values).length > 0) {
+        await influx.write({meas: 'dns-network', values});
+    }
 };

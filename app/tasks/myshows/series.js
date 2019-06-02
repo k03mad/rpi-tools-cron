ea5c3e2ac1c79de6c76e940d24067f7a3b0c58fb
k03mad <k03.mad@gmail.com>
2019-06-03 00:06:54 +0300

    update utils

--- app/tasks/myshows/series.js
+++ app/tasks/myshows/series.js
@@ -3,16 +3,11 @@
 const {myshows, influx} = require('utils-mad');
 
 module.exports = async () => {
-    const body = await myshows.get({method: 'profile.Shows'});
-    const watching = body.filter(elem => elem.watchStatus === 'watching');
+    const body = await myshows.watch();
 
     const values = {};
-    watching.forEach(elem => {
-        const remaining = elem.totalEpisodes - elem.watchedEpisodes;
-
-        if (remaining > 0) {
-            values[elem.show.title] = remaining;
-        }
+    body.forEach(elem => {
+        values[elem.show.title] = elem.totalEpisodes - elem.watchedEpisodes;
     });
 
     await influx.write({meas: 'myshows-series', values});

7c311a5541ffdf826fc5333e024e6d266c59b083
k03mad <k03.mad@gmail.com>
2019-06-13 23:45:14 +0300

    deps update

--- app/lib/schedule.js
+++ app/lib/schedule.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const cron = require('node-cron');
-const {array, date} = require('utils-mad');
+const {array, print} = require('utils-mad');
 
 module.exports = crons => {
     for (const [key, value] of Object.entries(crons)) {
         for (const func of array.convert(value)) {
             cron.schedule(
                 key,
-                () => func().catch(err => console.error(`${date.now()}\n${err}`)),
+                () => func().catch(err => print.ex(err)),
                 {timezone: 'Europe/Moscow'},
             );
         }
--- runner.js
+++ runner.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const tasks = require('require-all')(`${__dirname}/app/tasks`);
+const {print} = require('utils-mad');
 
 const [section, name] = JSON.parse(process.env.npm_config_argv).remain;
 const task = tasks[section][name];
 
 task()
-    .then(console.log)
-    .catch(err => console.log(err));
+    .then(msg => print.log(msg))
+    .catch(err => print.ex(err));

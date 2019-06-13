426c8c3a31c81c176b8182d34469880eb75cecaa
k03mad <k03.mad@gmail.com>
2019-06-13 03:13:01 +0300

    update deps

--- app/lib/schedule.js
+++ app/lib/schedule.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const cron = require('node-cron');
-const {array, log} = require('utils-mad');
+const {array, date} = require('utils-mad');
 
 module.exports = crons => {
     for (const [key, value] of Object.entries(crons)) {
         for (const func of array.convert(value)) {
             cron.schedule(
                 key,
-                () => func().catch(err => log.print(err)),
+                () => func().catch(err => console.error(`${date.now()}\n${err}`)),
                 {timezone: 'Europe/Moscow'},
             );
         }

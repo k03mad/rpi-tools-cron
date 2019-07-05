8cfa21a523392da310ef4b7a8bc4886f3ace2f52
k03mad <k03.mad@gmail.com>
2019-07-06 02:21:05 +0300

    update repo script

--- app/tasks/dns/update.js
+++ app/tasks/dns/update.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const appRoot = require('app-root-path');
-const {influx, shell} = require('utils-mad');
+const {influx, repo} = require('utils-mad');
 const {promises: fs} = require('fs');
 
 module.exports = async () => {
     const REPO = 'adblock-hosts-list';
     const file = `${REPO}/output/stats.json`;
 
-    await shell.script(REPO, 'deploy && pihole -g');
+    await repo.run(REPO, 'deploy && pihole -g');
     const data = await fs.readFile(`${appRoot}/../${file}`, 'utf8');
 
     const values = {};

23d31ff4b9a4635676f09ae2bc9353fc10e6cf17
k03mad <k03.mad@gmail.com>
2019-10-22 04:29:04 +0300

    parse stats

--- app/tasks/tools/parse.js
+++ app/tasks/tools/parse.js
@@ -1,5 +1,22 @@
 'use strict';
 
-const {repo} = require('utils-mad');
+const appRoot = require('app-root-path');
+const {promises: fs} = require('fs');
+const {repo, influx} = require('utils-mad');
 
-module.exports = () => repo.run('magnet-co-parser', 'start');
+module.exports = async () => {
+    const NAME = 'magnet-co-parser';
+    await repo.run(NAME, 'start');
+
+    const files = [
+        `${NAME}/parsed/films.json`,
+        `${NAME}/parsed/shows.json`,
+    ];
+
+    const [films, shows] = await Promise.all(files.map(async file => {
+        const data = await fs.readFile(`${appRoot}/../${file}`, 'utf8');
+        return JSON.parse(data).timestamp.diffRaw;
+    }));
+
+    await influx.write({meas: 'parse-stats', values: {films, shows}});
+};

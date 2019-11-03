5ebd57801c92e0dc14250b7ded7034865c8bb3eb
k03mad <k03.mad@gmail.com>
2019-11-04 02:16:32 +0300

    .

--- app/tasks/dns/stats.js
+++ app/tasks/dns/stats.js
@@ -19,6 +19,7 @@ module.exports = async () => {
     await Promise.all([
         influx.write({meas: 'dns-stats-common', values: {avg_processing_time, num_blocked_filtering, num_dns_queries}}),
         influx.write({meas: 'dns-stats-clients', values: array.mergeobj(top_clients)}),
+
         influx.write({meas: 'dns-stats-domains-q', values: array.mergeobj(top_queried_domains.slice(0, DOMAINS_COUNT))}),
         influx.write({meas: 'dns-stats-domains-b', values: array.mergeobj(top_blocked_domains.slice(0, DOMAINS_COUNT))}),
     ]);

3c5a0c4c469a9976a63ea8cd7f158be51cd232a0
k03mad <k03.mad@gmail.com>
2019-11-04 03:12:08 +0300

    stats update

--- app/tasks/dns/stats.js
+++ app/tasks/dns/stats.js
@@ -6,19 +6,49 @@ const {sendAdgRequest} = require('../../lib/api');
 module.exports = async () => {
     const DOMAINS_COUNT = 15;
 
-    const {
-        avg_processing_time,
-        num_blocked_filtering,
-        num_dns_queries,
+    const [
+        {
+            avg_processing_time,
+            num_blocked_filtering,
+            num_dns_queries,
 
-        top_blocked_domains,
-        top_queried_domains,
-        top_clients,
-    } = await sendAdgRequest('stats');
+            top_blocked_domains,
+            top_queried_domains,
+            top_clients,
+        }, {
+            clients,
+        },
+    ] = await Promise.all([
+        sendAdgRequest('stats'),
+        sendAdgRequest('clients'),
+    ]);
+
+    const clientsNamed = {};
+    top_clients.forEach(elem => {
+        const [[ip, count]] = Object.entries(elem);
+        let found = false;
+
+        if (ip.startsWith('10.')) {
+            clientsNamed[`VPN (${ip})`] = count;
+            found = true;
+        } else {
+            for (const client of clients) {
+                if (client.ip === ip) {
+                    clientsNamed[`${client.name} (${ip})`] = count;
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        if (!found) {
+            clientsNamed[`_Unknown (${ip})`] = count;
+        }
+    });
 
     await Promise.all([
         influx.write({meas: 'dns-stats-common', values: {avg_processing_time, num_blocked_filtering, num_dns_queries}}),
-        influx.write({meas: 'dns-stats-clients', values: array.mergeobj(top_clients)}),
+        influx.write({meas: 'dns-stats-clients', values: clientsNamed}),
 
         influx.write({meas: 'dns-stats-domains-q', values: array.mergeobj(top_queried_domains.slice(0, DOMAINS_COUNT))}),
         influx.write({meas: 'dns-stats-domains-b', values: array.mergeobj(top_blocked_domains.slice(0, DOMAINS_COUNT))}),

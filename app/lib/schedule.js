e84c5e14ea9f5220ab9dd75a6625398c94748978
Kirill Molchanov <k03.mad@gmail.com>
2019-11-06 11:44:59 +0300

    more logs

--- app/lib/schedule.js
+++ app/lib/schedule.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const cron = require('node-cron');
-const {array, print} = require('utils-mad');
+const {array, print, date} = require('utils-mad');
 
 module.exports = crons => {
     for (const [key, value] of Object.entries(crons)) {
@@ -9,11 +9,16 @@ module.exports = crons => {
             cron.schedule(
                 key,
                 () => {
-                    console.log(`fired ${key}`);
-                    return func().catch(err => print.ex(err, {
-                        add: `cron "${key}"`,
-                        exit: true,
-                    }));
+                    const id = Math.floor(Math.random() * Math.floor(100000);
+                    const now = date.now();
+
+                    console.log(`before: cron "${key}" id "${id}" date "${now}"`);
+                    return func()
+                        .then(console.log(`then: cron "${key}" id "${id}" date "${now}"`));
+                        .catch(err => print.ex(err, {
+                            add: `catch: cron "${key}" id "${id}" date "${now}"`,
+                            exit: true,
+                        }));
                 },
                 {timezone: 'Europe/Moscow'},
             );

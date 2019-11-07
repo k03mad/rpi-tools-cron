4e8d32d6185ec9b346612480f8beeae6fc0ecad0
k03mad <k03.mad@gmail.com>
2019-11-07 17:55:11 +0300

    crons

--- app/lib/schedule.js
+++ app/lib/schedule.js
@@ -1,34 +1,14 @@
 'use strict';
 
 const cron = require('node-cron');
-const {array, print, date} = require('utils-mad');
+const {array, print} = require('utils-mad');
 
 module.exports = crons => {
     for (const [key, value] of Object.entries(crons)) {
         for (const func of array.convert(value)) {
             cron.schedule(
                 key,
-                () => {
-                    const id = Math.floor(Math.random() * Math.floor(100000));
-                    const skip = key === '* * * * *';
-
-                    if (!skip) {
-                        console.log(`before: ${id} "${key}" ${date.now()}`);
-                    }
-
-                    return func()
-                        .then(() => {
-                            if (!skip) {
-                                console.log(`then: ${id} "${key}" ${date.now()}`);
-                            }
-
-                            return true;
-                        })
-                        .catch(err => print.ex(err, {
-                            add: `catch: ${id} "${key}" ${date.now()}`,
-                            exit: true,
-                        }));
-                },
+                () => func().catch(err => print.ex(err, {exit: true})),
                 {timezone: 'Europe/Moscow'},
             );
         }

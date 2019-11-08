1d5ea0d5dfb4b10d26bc3af921a0fa7633896d6d
k03mad <k03.mad@gmail.com>
2019-11-08 19:29:38 +0300

    update dns

--- app/tasks/dns/update.js
+++ app/tasks/dns/update.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const appRoot = require('app-root-path');
-const {influx, repo, array} = require('utils-mad');
+const {influx, repo} = require('utils-mad');
 const {promises: fs} = require('fs');
 const {sendAdgRequest} = require('../../lib/api');
 
@@ -13,22 +13,21 @@ module.exports = async () => {
     await sendAdgRequest('filtering/refresh', {method: 'POST', json: false});
 
     const apiData = await sendAdgRequest('filtering/status');
-    const apiHostsSum = array.sum(apiData.filters.map(elem => elem.rules_count));
-
     const fileData = JSON.parse(await fs.readFile(`${appRoot}/../${file}`, 'utf8'));
 
     const listsValues = {};
 
     Object.entries(fileData.results).forEach(([key, value]) => {
-        if (listsValues[key]) {
-            key = `dup!! ${key}`;
-        }
-
         listsValues[key] = value;
     });
 
     await Promise.all([
         influx.write({meas: 'dns-lists', values: listsValues}),
-        influx.write({meas: 'dns-lists-count', values: {api: apiHostsSum, file: fileData.uniqcount, white: fileData.whitelisted}}),
+        influx.write({meas: 'dns-lists-count', values: {
+            api: apiData.filters[0].rules_count,
+            fileHosts: fileData.hostscount,
+            fileDomains: fileData.domainscount,
+            white: fileData.whitelisted,
+        }}),
     ]);
 };

1f44f9d54ae003935c8d567b83382933b0d335be
k03mad <k03.mad@gmail.com>
2020-01-22 01:29:19 +0300

    dns stats

---
+++ app/tasks/router/clients.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const oui = require('oui');
+const {influx, mikrotik} = require('utils-mad');
+
+module.exports = async () => {
+    const SEPARATOR = ' :: ';
+
+    const clientsSignal = {};
+    const clientsTraffic = {};
+
+    const [
+        interfaces,
+        wifiClients,
+        dhcpLeases,
+    ] = await mikrotik.get([
+        '/interface/print',
+        '/interface/wireless/registration-table/print',
+        '/ip/dhcp-server/lease/print',
+    ]);
+
+    wifiClients.forEach(elem => {
+        const dbm = elem['signal-strength'].replace(/@.+/, '');
+        const trf = elem.bytes.replace(',', '.');
+        const mac = elem['mac-address'];
+
+        const [client] = dhcpLeases.filter(lease => lease['mac-address'] === mac);
+
+        let key;
+
+        if (client && client.comment) {
+            key = client.comment;
+        } else {
+            const [vendor] = oui(mac).split('\n')[0].match(/^(\w+( \w+)?)/);
+            key = vendor + SEPARATOR + mac;
+        }
+
+        clientsTraffic[key] = Number(trf);
+        clientsSignal[key] = Number(dbm);
+    });
+
+    interfaces.forEach(elem => {
+        const sum = Number(elem['rx-byte']) + Number(elem['tx-byte']);
+
+        if (elem.name.includes('ether') && sum > 0) {
+            clientsTraffic[elem.name] = sum;
+        }
+    });
+
+    await influx.append({meas: 'router-clients-traffic', values: clientsTraffic});
+    await influx.write({meas: 'router-clients-signal', values: clientsSignal});
+};

c6898029da6d1f361b37bc2fd817bc1dc831eda0
k03mad <k03.mad@gmail.com>
2020-02-12 03:02:51 +0300

    myshows last 5 years

--- app/tasks/myshows/year.js
+++ app/tasks/myshows/year.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const {array} = require('utils-mad');
 const {influx, date, parse, ua} = require('utils-mad');
 
 module.exports = async () => {
@@ -10,28 +11,32 @@ module.exports = async () => {
         current,
         previous: current - 1,
         next: current + 1,
+        last5: [2, 3, 4, 5, 6].map(elem => current - elem),
     };
 
     for (const year of Object.keys(dates)) {
-        const parsed = await parse.text({
-            selector: '.catalogTable .alignRight:nth-child(3) , td a',
-            url: `https://myshows.me/search/all/?year=${dates[year]}`,
-            gotOpts: {
-                headers: {'user-agent': ua.random.desktop()},
-                timeout: 30000,
-            },
-        });
+        const series = {};
 
-        const values = {};
+        for (const elem of array.convert(dates[year])) {
+            const parsed = await parse.text({
+                selector: '.catalogTable .alignRight:nth-child(3) , td a',
+                url: `https://myshows.me/search/all/?year=${elem}`,
+                gotOpts: {
+                    headers: {'user-agent': ua.random.desktop()},
+                    timeout: 30000,
+                },
+            });
 
-        for (let i = 0; i < parsed.length && i < SHOWS_COUNT * 2; i += 2) {
-            values[parsed[i]] = Number(parsed[i + 1].replace(/\s/g, ''));
+            for (let i = 0; i < parsed.length; i += 2) {
+                series[parsed[i]] = Number(parsed[i + 1].replace(/\s/g, ''));
+            }
+
+            if (year === 'next' && Object.keys(series).length === 0) {
+                continue;
+            }
         }
 
-        if (year === 'next' && Object.keys(values).length === 0) {
-            continue;
-        }
-
+        const values = Object.fromEntries(Object.entries(series).slice(0, SHOWS_COUNT));
         await influx.write({meas: `myshows-year-${year}`, values});
     }
 };

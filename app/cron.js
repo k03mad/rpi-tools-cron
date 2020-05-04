47fa4ebf771b7cc410bada2097cad0b69c6e7948
k03mad <k03.mad@gmail.com>
2020-05-04 22:49:55 +0300

    remove prune

--- app/cron.js
+++ app/cron.js
@@ -14,8 +14,6 @@ const timers = {
         pi.usage,
     ],
 
-    '30 3 * * *': pi.prune,
-
     '15 4,5 * * *': mik.pptp,
     '30 4,5 * * *': magnet.parse,
 };
--- app/tasks/pi/prune.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const {shell} = require('utils-mad');
-
-module.exports = () => shell.run('pnpm prune store');

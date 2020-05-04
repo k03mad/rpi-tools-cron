de1828eb68da395cda8be26eb95e3e820f1b67c4
k03mad <k03.mad@gmail.com>
2020-05-04 21:48:23 +0300

    update deps

--- app/tasks/dns/stats.js
+++ app/tasks/dns/stats.js
@@ -60,9 +60,11 @@ module.exports = async () => {
     await influx.write([
         {meas: 'dns-stats-common', values: {avg_processing_time, num_blocked_filtering, num_dns_queries}},
         {meas: 'dns-stats-clients-internal', values: internal},
-        {meas: 'dns-stats-clients-external', values: external},
         {meas: 'dns-stats-domains-q', values: array.mergeCol(top_queried_domains.slice(0, DOMAINS_COUNT))},
         {meas: 'dns-stats-domains-b', values: array.mergeCol(top_blocked_domains.slice(0, DOMAINS_COUNT))},
     ]);
 
+    if (Object.keys(external).length > 0) {
+        await influx.write({meas: 'dns-stats-clients-external', values: external});
+    }
 };

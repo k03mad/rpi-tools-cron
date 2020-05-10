56fbc370ec1fa0379954e3619ea194a0223c6c85
k03mad <k03.mad@gmail.com>
2020-05-10 19:06:32 +0300

    stats update

--- app/tasks/dns/stats.js
+++ app/tasks/dns/stats.js
@@ -50,7 +50,6 @@ module.exports = async () => {
                 lookup.countrycode,
                 lookup.city,
                 lookup.isp,
-                address,
             ].join(IP_SEPARATOR);
 
             object.count(external, clientInfo, count);

3ba9d20e7fcef81db1fe6b86e508d40b4af557fb
k03mad <k03.mad@gmail.com>
2020-07-02 22:58:18 +0300

    add cron names

--- app/cron.js
+++ app/cron.js
@@ -1,38 +1,46 @@
 'use strict';
 
-const {array, print, repo, shell, influx} = require('utils-mad');
 const {Cron} = require('recron');
+const {print, repo, shell, influx} = require('utils-mad');
 
 const tasks = {
-    '* * * * *': [
-        require('./tasks/adguard'),
-        require('./tasks/mikrotik'),
-        require('./tasks/pi'),
-    ],
-
-    '* */6 * * *': async () => {
-        const apt = await shell.run([
-            'sudo apt-get update',
-            'sudo apt-get upgrade -u -s',
-        ]);
-
-        const updates = apt.split('\n').filter(el => el.includes('Inst')).length;
-        await influx.write({meas: 'pi-updates', values: {count: `Updates: ${updates}`}});
+    '* * * * *': {
+        adg: require('./tasks/adguard'),
+        mik: require('./tasks/mikrotik'),
+        pi: require('./tasks/pi'),
     },
 
-    '0 5 * * *': () => repo.run('magnet-co-parser', 'start'),
+    '* */6 * * *': {
+        apt: async () => {
+            const apt = await shell.run([
+                'sudo apt-get update',
+                'sudo apt-get upgrade -u -s',
+            ]);
+
+            const updates = apt.split('\n').filter(el => el.includes('Inst')).length;
+            await influx.write({meas: 'pi-updates', values: {count: `Updates: ${updates}`}});
+        },
+    },
+
+    '0 5 * * *': {
+        parse: () => repo.run('magnet-co-parser', 'start'),
+    },
 };
 
 const cron = new Cron();
 cron.start();
 
 for (const [key, value] of Object.entries(tasks)) {
-    for (const func of array.convert(value)) {
+    for (const [name, func] of Object.entries(value)) {
         cron.schedule(key, async () => {
             try {
                 await func();
             } catch (err) {
-                print.ex(err, {before: key, afterline: false, exit: true});
+                print.ex(err, {
+                    before: `${key} :: ${name}`,
+                    afterline: false,
+                    exit: true,
+                });
             }
         }, {timezone: 'Europe/Moscow'});
     }

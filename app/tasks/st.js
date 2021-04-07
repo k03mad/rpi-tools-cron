87ae7d2d6886a9570e7aeea1c8b923c37511ae34
k03mad <k03.mad@gmail.com>
2021-04-07 22:48:33 +0300

    use speed-cloudflare-cli

--- app/tasks/st.js
+++ app/tasks/st.js
@@ -1,28 +1,29 @@
 'use strict';
 
+const stripAnsi = require('strip-ansi');
 const {shell, influx} = require('@k03mad/utils');
 
 /***/
 module.exports = async () => {
-    const st = await shell.run('speed-test -v');
-    const matched = st.match(/ping\s+(?<ping>\d+)[\S\s]+download\s+(?<download>\d+)[\S\s]+upload\s+(?<upload>\d+)[\S\s]+location\s+(?<location>.+) [\S\s]+distance\s+(?<distance>[\d.]+)/i);
+    const st = await shell.run('speed-cloudflare-cli');
+    const data = stripAnsi(st)
+        .split('\n')
+        .map(elem => elem.trim());
 
-    if (!matched) {
-        throw new Error(`Speed-Test results doesn't match to regexp\n\n${st}`);
-    }
-
-    const {download, upload, ping, location, distance} = matched.groups;
-
-    const locSantzd = location.replace('tischi', 'tishchi');
+    const location = data[0].replace('Server location: ', '');
+    const latency = data[2].replace(/Latency: | ms/g, '');
+    const jitter = data[3].replace(/Jitter: | ms/g, '');
+    const download = data[9].replace(/Download speed: | Mbps/g, '');
+    const upload = data[10].replace(/Upload speed: | Mbps/g, '');
 
     await influx.write([
         {meas: 'pi-speed-test-mbps', values: {
-            [` ↓ ${locSantzd}`]: Number(download),
-            [` ↑ ${locSantzd}`]: Number(upload),
+            [` ↓ ${location}`]: Number(download),
+            [` ↑ ${location}`]: Number(upload),
         }},
         {meas: 'pi-speed-test-data', values: {
-            ping: Number(ping),
-            distance: Number(distance),
+            latency: Number(latency),
+            jitter: Number(jitter),
         }},
     ]);
 };

3eba8a9ae9ee4ba1eeb7ab2fb03b549bafb7d005
k03mad <k03.mad@gmail.com>
2021-05-11 16:15:59 +0300

    add error catch

--- app/tasks/next-sync.js
+++ app/tasks/next-sync.js
@@ -3,4 +3,12 @@
 const {repo} = require('@k03mad/utils');
 
 /** @returns {Promise<string>} */
-module.exports = () => repo.run('nextdns-lists-sync', 'upload');
+module.exports = async () => {
+    try {
+        await repo.run('nextdns-lists-sync', 'upload');
+    } catch (err) {
+        if (!err.includes('nothing to commit')) {
+            throw err;
+        }
+    }
+};

96382fc91ffe2d351dbc0ae5da87861b14c42dd4
k03mad <k03.mad@gmail.com>
2021-07-06 13:26:57 +0300

    fix request

--- app/tasks/request.js
+++ app/tasks/request.js
@@ -3,31 +3,29 @@
 const globby = require('globby');
 const os = require('os');
 const path = require('path');
-const {influx, print} = require('@k03mad/utils');
+const {influx} = require('@k03mad/utils');
 const {promises: fs} = require('fs');
 
 /***/
 module.exports = async () => {
     const requestFiles = await globby(path.join(os.tmpdir(), '_req_stats'));
 
-    for (const file of requestFiles) {
+    const data = await Promise.all(requestFiles.map(async file => {
         const content = await fs.readFile(file, {encoding: 'utf-8'});
-        let parsed;
 
         try {
-            parsed = JSON.parse(content);
-        } catch (err) {
-            print.ex(err, {before: `Cannot parse request data\n${file}`});
-            continue;
-        }
+            const {statusCode, method, domain, timing, date} = JSON.parse(content);
 
-        const {statusCode, method, domain, timing, date} = parsed;
-        await influx.write({
-            meas: 'pi-node-request',
-            values: {[`${statusCode} ${method} ${domain}`]: timing},
-            timestamp: date,
-        });
+            return {
+                meas: 'pi-node-request',
+                values: {[`${statusCode} ${method} ${domain}`]: timing},
+                timestamp: date,
+            };
+        } catch {}
 
         await fs.unlink(file);
-    }
+        return null;
+    }));
+
+    await influx.write(data.filter(Boolean));
 };

324c0ceef93cdaa4f0d04ad9aa858d4536c607ea
k03mad <k03.mad@gmail.com>
2021-07-09 18:21:19 +0300

    pretty

--- app/tasks/pinger.js
+++ app/tasks/pinger.js
@@ -18,7 +18,7 @@ module.exports = async () => {
 
         if (lastCheck !== online) {
             const status = online ? 'UP' : 'DOWN';
-            await pinger.notify({text: `pi ping: \`${status}\` ${option.join(':')}`});
+            await pinger.notify({text: `\`${status}\` ${option.join(':')} (from pi)`});
 
             lastCheck = online;
         }

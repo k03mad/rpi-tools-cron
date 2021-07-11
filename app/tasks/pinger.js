173d51e0080a1f519d7651c4c8c3734ee273850e
k03mad <k03.mad@gmail.com>
2021-07-11 16:53:57 +0300

    update deps

--- app/tasks/pinger.js
+++ app/tasks/pinger.js
@@ -2,25 +2,9 @@
 
 const {cloud} = require('../../env');
 const {pinger} = require('@k03mad/utils');
-const {tcpPingPort} = require('tcp-ping-port');
 
-let lastCheck;
-
-/***/
-module.exports = async () => {
-    const options = [
-        [cloud.domain, 22],
-        [cloud.ip, 22],
-    ];
-
-    await Promise.all(options.map(async option => {
-        const {online} = await tcpPingPort(...option);
-
-        if (lastCheck !== online) {
-            const status = online ? 'UP' : 'DOWN';
-            await pinger.notify({text: `\`${status}\` ${option.join(':')} (from pi)`});
-
-            lastCheck = online;
-        }
-    }));
-};
+/** @returns {Promise} */
+module.exports = () => pinger.check([
+    {domain: cloud.domain, port: 22},
+    {domain: cloud.ip, port: 22},
+]);

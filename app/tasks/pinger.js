d5122325be330103f9098bf75bc16e653668c7e5
k03mad <k03.mad@gmail.com>
2021-07-11 17:07:05 +0300

    update pinger

--- app/tasks/pinger.js
+++ app/tasks/pinger.js
@@ -4,7 +4,4 @@ const {cloud} = require('../../env');
 const {pinger} = require('@k03mad/utils');
 
 /** @returns {Promise} */
-module.exports = () => pinger.check([
-    {domain: cloud.domain, port: 22},
-    {domain: cloud.ip, port: 22},
-]);
+module.exports = () => pinger.check({domain: cloud.domain, port: 22});

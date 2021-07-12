beca1090551e7cd77137d2405c7917fb224d9013
Kirill Molchanov <k03.mad@gmail.com>
2021-07-13 00:03:24 +0300

    change port

--- app/tasks/pinger.js
+++ app/tasks/pinger.js
@@ -4,4 +4,4 @@ const {cloud} = require('../../env');
 const {pinger} = require('@k03mad/utils');
 
 /** @returns {Promise} */
-module.exports = () => pinger.check({domain: cloud.domain, port: 22});
+module.exports = () => pinger.check({domain: cloud.domain, port: cloud.port});
